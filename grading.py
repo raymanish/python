b906f3078455a017fed4ed87a7e4172df761fa61
raymanish <161663390+raymanish@users.noreply.github.com>
2024-04-19 14:20:59 +1200

    Update grading.py

--- grading.py
+++ grading.py
@@ -1,10 +1,12 @@
-a= int(input("enter the marks of 1st subject:"))
-b= int(input("enter the marks of 2nd subject:"))
-c= int(input("enter the marks of 3rd subject:"))
-d= int(input("enter the marks of 4th subject:"))
+# Function for use: This script calculates the average marks of four subjects and determines whether the student has passed or failed.
+a = int(input("Enter the marks of 1st subject:"))
+b = int(input("Enter the marks of 2nd subject:"))
+c = int(input("Enter the marks of 3rd subject:"))
+d = int(input("Enter the marks of 4th subject:"))
 
-average_marks=(a+b+c+d/4)
-if average_marks>=50:
-    print("you have failed")
+average_marks = (a + b + c + d) / 4
+
+if average_marks >= 50:
+    print("You have failed")
 else:
-    print("you have passed")
+    print("You have passed")
