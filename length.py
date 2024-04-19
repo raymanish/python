208572f3d0bf4826176bea8a43190db72f5f5085
raymanish <161663390+raymanish@users.noreply.github.com>
2024-04-19 14:22:37 +1200

    Update length.py

--- length.py
+++ length.py
@@ -1,9 +1,11 @@
-m1=int(input("enter the score"))
-m2=int(input("enter the score"))
+# Function for use: This script calculates the average score of two subjects and determines whether the average score indicates a pass or fail.
+m1 = int(input("Enter the score for the 1st subject:"))
+m2 = int(input("Enter the score for the 2nd subject:"))
 
-total=m1+m2
-avg= total/2
-if avg>=50:
-    print("pass")
+total = m1 + m2
+avg = total / 2
+
+if avg >= 50:
+    print("Pass")
 else:
-    print("fail")
+    print("Fail")
