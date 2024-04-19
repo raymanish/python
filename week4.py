e9ed5e33f894d0eec74c0fe347c8a8ea7c28b5ac
raymanish <161663390+raymanish@users.noreply.github.com>
2024-04-19 14:33:00 +1200

    Update week4.py

--- week4.py
+++ week4.py
@@ -1,6 +1,8 @@
-msg=str(input("enter your message:"))
-count=0
+msg = str(input("Enter your message: "))
+count = 0
 for i in msg:
-    if i== 'a'or i=='e' or i=='i' or i=='o' or i=='u':
-        count +=1
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+        count += 1
         print(count)
+
+# Function for use: This script counts the number of vowels (a, e, i, o, u) in a given message and prints the count for each vowel encountered.
