a2087dd06feb03f6b21a5cfbc664b54b6315487d
raymanish <161663390+raymanish@users.noreply.github.com>
2024-04-19 14:25:35 +1200

    Update random.py

--- random.py
+++ random.py
@@ -1,7 +1,6 @@
 import random
 
-# using random.sample()
-# to generate random number list
+# Function for use: This script generates a list of 18 unique random numbers between 1 and 20 using the random.sample() function.
 res = random.sample(range(1, 21), 18)
 
 print(res)
